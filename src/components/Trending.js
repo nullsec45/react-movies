c8d3a42212200a3bcaf043069d4e0276b7853db5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-04-02 02:27:09 +0700

    komponen trending statis jadi dinamis

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,74 +1,43 @@
-import {Card,Col, Row, Container} from "react-bootstrap";
-import duneImage from "../assets/images/trending/dune.jpg";
-import everythingImage from "../assets/images/trending/everything.jpg";
-import infiniteImage from "../assets/images/trending/infinite.jpg";
-import jokerImage from "../assets/images/trending/joker.jpg";
-const Trending=() =>
-{
+import { Card, Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+
+const Trending = () => {
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		getMovies();
+	}, []);
+
+	const getMovies = async () => {
+		const response = await axios.get("http://localhost:5000/movies");
+		// console.log(response.data[1]);
+		setMovies(response.data[0]);
+	}
 	return (
 		<div>
 			<Container>
 				<Row id="trending">
-					<Col> 
-					   <h1 className="text-center text-white mt-4">Trending Movies</h1>
+					<Col>
+						<h1 className="text-center text-white mt-4">Trending Movies</h1>
 					</Col>
 				</Row>
 				<Row className="mt-4" >
-				   {/*film 1*/}
-					<Col xs={3}>
-					  <Card>
-					      <Card.Img variant="top" src={duneImage} className="imgMovie" />
-					      <Card.Body>
-					        <Card.Title>Card Title</Card.Title>
-					        <Card.Text>
-					          Some quick example text to build on the card title and make up the
-					          bulk of the card's content.
-					        </Card.Text>
-					      </Card.Body>
-					   </Card>
-					</Col>
-
-				    {/*film2*/}
-					<Col xs={3}>
-					  <Card>
-					      <Card.Img variant="top" src={everythingImage} className="imgMovie" />
-					      <Card.Body>
-					        <Card.Title>Card Title</Card.Title>
-					        <Card.Text>
-					          Some quick example text to build on the card title and make up the
-					          bulk of the card's content.
-					        </Card.Text>
-					      </Card.Body>
-					   </Card>
-					</Col>
-
-				   {/*film 3*/}
-				   <Col xs={3}>
-					  <Card>
-					      <Card.Img variant="top" src={infiniteImage} className="imgMovie" />
-					      <Card.Body>
-					        <Card.Title>Card Title</Card.Title>
-					        <Card.Text>
-					          Some quick example text to build on the card title and make up the
-					          bulk of the card's content.
-					        </Card.Text>
-					      </Card.Body>
-					   </Card>
-					</Col>
-
-					{/*film 4*/}
-					<Col xs={3}>
-					  <Card>
-					      <Card.Img variant="top" src={jokerImage} className="imgMovie" />
-					      <Card.Body>
-					        <Card.Title>Card Title</Card.Title>
-					        <Card.Text>
-					          Some quick example text to build on the card title and make up the
-					          bulk of the card's content.
-					        </Card.Text>
-					      </Card.Body>
-					   </Card>
-					</Col>
+					{/*film 1*/}
+					{movies.map((movie, index) => (
+						<Col xs={3} key={index}>
+							<Card>
+								<Card.Img variant="top" src={movie.image} className="imgMovie" />
+								<Card.Body>
+									<Card.Title>{movie.title}</Card.Title>
+									<Card.Text>
+										{movie.body}
+									</Card.Text>
+								</Card.Body>
+							</Card>
+						</Col>
+					))}
 				</Row>
 			</Container>
 		</div>
